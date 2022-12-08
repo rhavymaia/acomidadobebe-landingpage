641d23a66bb230b0b7ec3929f9383f431543bf31
Thiágo Morais <35998529+moraisthiago@users.noreply.github.com>
2022-12-08 14:40:00 -0300

    Build Automático 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
         <Container>
           <Navbar.Brand>
             <Logo />
-            Jenkins Build Teste - Load Balancer - Build Automático 1
+            Jenkins Build Teste - Load Balancer - Build Automático 2
           </Navbar.Brand>
 
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
